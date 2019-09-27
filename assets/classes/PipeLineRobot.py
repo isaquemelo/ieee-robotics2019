501e5a978172f8563e890fa225011093e62dfa3c
Isaque Melo <isaquegbmelo2@gmail.com>
2019-09-26 21:52:03 -0300

    found a way to use upper_dist with ultrasonic

--- assets/classes/PipeLineRobot.py
+++ assets/classes/PipeLineRobot.py
@@ -47,8 +47,8 @@ class PipeLineRobot:
             7: 'Brown'
         }
 
-        self.ultrasonic_sensors = {"bottom": 2550, "left": 2550}
-        self.infrared_sensors = {"diagonal_top": ev3.InfraredSensor('in3'), "right": 50, "left": 50, "frontal": 100}
+        self.ultrasonic_sensors = {"left": 2550, "diagonal_top": ev3.UltrasonicSensor('in3')}
+        self.infrared_sensors = {"right": 50, "left": 50, "frontal": 100}
 
         # define motors
         self.motors = Duo(ev3.LargeMotor('outB'), ev3.LargeMotor('outD'))
@@ -94,18 +94,17 @@ class PipeLineRobot:
         # returns the value of a sensor
 
         if sensor_name == "InfraredSensor":
-            return [self.infrared_sensors["left"], self.infrared_sensors["right"],
-                    self.infrared_sensors["diagonal_top"].value()]
-            # return self.ultrasonic_sensors["front-left"].value() / 10, self.ultrasonic_sensors[
-            #     "front-right"].value() / 10, \
-            #        self.infrared_sensors["upper_front"]
+            # return [self.infrared_sensors["left"], self.infrared_sensors["right"],
+            #         self.infrared_sensors["diagonal_top"].value()]
+            return [self.infrared_sensors["left"], self.infrared_sensors["right"]]
 
         elif sensor_name == "GyroSensor":
             return self.gyroscope_sensor.angle
 
         elif sensor_name == "Ultrasonic":
             # return self.ultrasonic_sensor.value() / 10
-            return [self.ultrasonic_sensors['left'] / 10, self.ultrasonic_sensors['bottom'] / 10]
+            # return [self.ultrasonic_sensors['left'] / 10, self.ultrasonic_sensors['bottom'] / 10]
+            return [self.ultrasonic_sensors['left'] / 10, self.ultrasonic_sensors["diagonal_top"].value() / 10]
 
         elif sensor_name == "ColorSensor":
             if ColorSensorMode != "COL-COLOR":
@@ -1038,3 +1037,139 @@ class PipeLineRobot:
 
             self.motors.left.run_forever(speed_sp=speed_a)
             self.motors.right.run_forever(speed_sp=speed_b)
+
+        self.color_sensors[0].mode = "COL-COLOR"
+        self.color_sensors[1].mode = "COL-COLOR"
+        return
+
+
+    def meeting_area_initial_setting(self):
+        self.color_sensors[0].mode = "COL-REFLECT"
+        self.color_sensors[1].mode = "COL-REFLECT"
+        default_speed = 400
+        max_speed = 700
+        min_speed = -max_speed
+        expected_side_dist = 30
+        k_to_find_abyss_by_upper_dist = 30
+        k_to_find_green_slope_by_upper_dist = 150
+        # when the sensor upper dist its on the meeting area it finds around 13 preety mush never dibber than 15
+        k_when_sensor_sees_the_flat_ground_by_upper_dist = 15
+        k_min_white_reflect = 50
+
+        while True:
+            left_dist = self.get_sensor_data("InfraredSensor")[0]
+            right_dist = self.get_sensor_data("InfraredSensor")[1]
+            upper_dist = self.get_sensor_data("Ultrasonic")[1]
+            color_data = self.get_sensor_data("ColorSensor", "r")
+
+            # find abyss
+            if upper_dist >= k_to_find_abyss_by_upper_dist:
+                self.stop_motors()
+                ev3.Sound.beep()
+                if upper_dist >= k_to_find_green_slope_by_upper_dist:
+                    print("found green slope")
+                    continue
+                else:
+                    print("found abyss")
+                    continue
+
+            # find robot
+
+            # find black
+            if color_data[0] < k_min_white_reflect or color_data[1] < k_min_white_reflect:
+                self.stop_motors()
+                ev3.Sound.beep()
+                self.alignment_for_meeting_area_initial_setting()
+                if upper_dist <= k_when_sensor_sees_the_flat_ground_by_upper_dist:
+                    print("found black line")
+                elif upper_dist > k_when_sensor_sees_the_flat_ground_by_upper_dist and \
+                    upper_dist < k_to_find_green_slope_by_upper_dist:
+                    print("found abyss")
+                elif upper_dist > k_when_sensor_sees_the_flat_ground_by_upper_dist and \
+                        upper_dist > k_to_find_green_slope_by_upper_dist:
+                    print("found green slope")
+                continue
+
+            if upper_dist < k_when_sensor_sees_the_flat_ground_by_upper_dist:
+                self.motors.left.run_forever(speed_sp=default_speed)
+                self.motors.right.run_forever(speed_sp=default_speed)
+            else:
+                self.motors.left.run_forever(speed_sp=default_speed / 2)
+                self.motors.right.run_forever(speed_sp=default_speed / 2)
+
+        self.color_sensors[0].mode = "COL-COLOR"
+        self.color_sensors[1].mode = "COL-COLOR"
+        return
+
+    def alignment_for_meeting_area_initial_setting(self):
+        default_speed = 200
+        k_min_white_reflect = 50
+        color_data = self.get_sensor_data("ColorSensor", "r")
+
+        while color_data[0] < k_min_white_reflect or color_data[1] < k_min_white_reflect:
+
+            while color_data[0] < k_min_white_reflect:
+                color_data = self.get_sensor_data("ColorSensor", "r")
+                self.motors.left.run_forever(speed_sp=-default_speed)
+            self.stop_motors()
+
+            while color_data[1] >= k_min_white_reflect:
+                pass
+            self.stop_motors()
+
+
+    def meeting_area_initial_setting1(self):
+        self.color_sensors[0].mode = "COL-REFLECT"
+        self.color_sensors[1].mode = "COL-REFLECT"
+        default_speed = 400
+        max_speed = 700
+        min_speed = -max_speed
+        expected_left_dist = 25
+        k_to_find_abyss_by_upper_dist = 30
+        k_to_find_green_slope_by_upper_dist = 150
+        # when the sensor upper dist its on the meeting area it finds around 13 preety mush never dibber than 15
+        k_when_sensor_sees_the_flat_ground_by_upper_dist = 15
+        k_min_white_reflect = 50
+        pid = PID(5, 0, 5, setpoint=0)
+
+        while True:
+            left_dist = self.get_sensor_data("InfraredSensor")[0]
+            upper_dist = self.get_sensor_data("Ultrasonic")[1]
+            color_data = self.get_sensor_data("ColorSensor", "r")
+
+            if upper_dist >= k_to_find_abyss_by_upper_dist:
+                self.stop_motors()
+                ev3.Sound.beep()
+                if upper_dist >= k_to_find_green_slope_by_upper_dist:
+                    print("found green slope")
+                else:
+                    print("found abbys")
+
+                continue
+
+            control = pid(expected_left_dist - left_dist)
+            print(control)
+
+            left_speed = default_speed + control
+            right_speed = default_speed - control
+
+            if left_speed > max_speed:
+                left_speed = max_speed
+            elif left_speed < min_speed:
+                left_speed = min_speed
+
+            if right_speed > max_speed:
+                right_speed = max_speed
+            elif right_speed < min_speed:
+                right_speed = min_speed
+
+            if upper_dist < k_when_sensor_sees_the_flat_ground_by_upper_dist:
+                self.motors.left.run_forever(speed_sp=left_speed)
+                self.motors.right.run_forever(speed_sp=right_speed)
+            else:
+                self.motors.left.run_forever(speed_sp=default_speed / 2)
+                self.motors.right.run_forever(speed_sp=default_speed / 2)
+
+        self.color_sensors[0].mode = "COL-COLOR"
+        self.color_sensors[1].mode = "COL-COLOR"
+        return
